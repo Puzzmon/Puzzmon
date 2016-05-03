2ffff608502d4fed98af6090c3b4aa721ef7b1ee
aserretaranda <aserretaranda@iesesteveterradas.cat>
2016-05-03 19:38:38 +0200

    Login 0.1
    empezando el loggin

--- index2.php
+++ index2.php
@@ -7,39 +7,47 @@ session_start();
 <head>
 	<meta charset="UTF-8">
 	<!-- Links Bootstrap css -->
-	<link rel="styleSheet" href="css/bootstrap.min.css"> 
-	<link rel="styleSheet" href="css/bootstrap.css">
+	<link rel="styleSheet" href="resources/css/bootstrap.min.css"> 
+	<link rel="styleSheet" href="resources/css/bootstrap.css">
 	<!-- Link css propio -->
-	<link rel="styleSheet" href="css/estilos.css">
+	<link rel="styleSheet" href="resources/css/estilos.css">
 	<!-- Link javascript y jquery -->
-	<script src="js/jquery.js"></script>
-	<script src="js/bootstrap.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+	<script src="resources/js/jquery.js"></script>
+	<script src="resources/js/bootstrap.js"></script>
+    <script src="resources/js/bootstrap.min.js"></script>
 	
 	<title>PuzzMon</title>
 </head>
 <body>
 	<div class="background">
-			<video src="AnimatronJaja.mp4" autoplay loop></video>
-		</div>
-	<div class="floatlogin">
-			<?php  
-			if (empty($_SESSION["username"])){
-				echo
-					'<form action="session.php", method="post">
-						<input class="loggin" type="text" name="username"></input>
-						<input class="loggin" type="password" name="password"></input>
-						<input class="logg" type="submit" name="logg" value="Entrar"></input>
-					</form>'
-				;
-			}
-			else {
-				echo '<div class="logphp">';
-				echo '<p>Bienvenido, <b>' . $_SESSION["username"] . '</b></p>';
-				echo '</div>';
-			}
-			?>
-		</div>
+		<video src="resources/src/AnimatronJaja.mp4" autoplay loop></video>
+	</div>
+	<?php  
+		if (empty($_SESSION["username"]))
+		{
+			echo"<div class='loggin'>";
+			echo"<form id='form' name='form' action='session.php' method='post'>";
+			echo"<div id='block'>";
+			echo"<label id='user' for='name'>p</label>";
+			echo"<input type='text' name='username' id='name' placeholder='Username' required/>";
+			echo"<label id='pass' for='password'>k</label>";
+			echo"<input type='password' name='password' id='password' placeholder='Password' required/>";
+			echo"<input type='submit' id='submit' name='logg' value='a'/>";
+			echo"</div>";
+			echo"</form>";
+			echo"<div id='option'>";
+			echo"<p>Sign in</p>";
+			echo"<a href='#'>forgot?</a>";
+			echo"</div>";
+			echo"</div>";
+		}
+		else
+		{
+			echo"<nav class='menu'>";
+			echo 'Bienvenido, <b>' . $_SESSION["username"] . '</b>';
+			echo '</nav>';
+		}
+	?>
 		
 	
 </body>
