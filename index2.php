07eaba6814d86550b6f361cfbac3f35fcd9a7ade
aserretaranda <aserretaranda@iesesteveterradas.cat>
2016-05-05 19:04:05 +0200

    jueves
    seguimos con le loggin

--- index2.php
+++ index2.php
@@ -15,7 +15,29 @@ session_start();
 	<script src="resources/js/jquery.js"></script>
 	<script src="resources/js/bootstrap.js"></script>
     <script src="resources/js/bootstrap.min.js"></script>
-	
+	<script type="text/javascript">
+	$(document).ready(function() {
+	    setTimeout(function() {
+	        $(".bienvenido").fadeOut(1500);
+	    },2000);
+	});
+	$(document).ready(function() {
+	    setTimeout(function() {
+	        $(".top").fadeIn(100);
+	    },3500);
+	});
+	</script>
+	<script type="text/javascript">
+	function openlogin(){
+		document.getElementById('buttons').style.display = 'none';
+		document.getElementById('loggin').style.display = 'block';
+	}
+	function openregister(){
+		document.getElementById('buttons').style.display = 'none';
+		document.getElementById('register').style.display = 'block';
+	}
+
+	</script>
 	<title>PuzzMon</title>
 </head>
 <body>
@@ -25,30 +47,42 @@ session_start();
 	<?php  
 		if (empty($_SESSION["username"]))
 		{
-			echo"<div class='loggin'>";
-			echo"<form id='form' name='form' action='session.php' method='post'>";
-			echo"<div id='block'>";
-			echo"<label id='user' for='name'>p</label>";
-			echo"<input type='text' name='username' id='name' placeholder='Username' required/>";
-			echo"<label id='pass' for='password'>k</label>";
-			echo"<input type='password' name='password' id='password' placeholder='Password' required/>";
-			echo"<input type='submit' id='submit' name='logg' value='a'/>";
-			echo"</div>";
-			echo"</form>";
-			echo"<div id='option'>";
-			echo"<p>Sign in</p>";
-			echo"<a href='#'>forgot?</a>";
-			echo"</div>";
-			echo"</div>";
+	?>
+			<div id="buttons"> 
+			<button class="btnlog" onclick="openlogin()">Log In</button>
+			<button class="btnregister" onlick="openregister()">Register</button> 
+
+			</div>
+	<?php
 		}
 		else
 		{
-			echo"<nav class='menu'>";
-			echo 'Bienvenido, <b>' . $_SESSION["username"] . '</b>';
-			echo '</nav>';
-		}
-	?>
-		
+	?>	
+			<div class='menu bienvenido'>
+				<p>Bienvenido, <b><?=$_SESSION["username"]?></b></p>
+			</div>
+			<div class='row menu top' style="display:none">
+				<div id="top" class="col-md-3 border"><b>Name: <?=$_SESSION["username"]?></b></div>
+				<div id="top" class="col-md-3 border"><b>Nivel: X</b></div>
+				<div id="top" class="col-md-offset-3 col-md-3 border"><a href="#">Log Out</a></div>
+			</div>
+	<?php		
+		}	
+		?>
+			<div id='loggin' style='display:none'>
+			<form id='form' name='form' action='session.php' method='post'>
+			<div id='block'>
+			<label id='user' for='name'>p</label>
+			<input type='text' name='username' id='name' placeholder='Username' required/>
+			<label id='pass' for='password'>k</label>
+			<input type='password' name='password' id='password' placeholder='Password' required/>
+			<input type='submit' id='submit' name='logg' value='a'/>
+			</div>
+			</form>
+			<div id='option'>
+			<a href='#'>forgot?</a>
+			</div>
+			</div>
 	
 </body>
 </html>
